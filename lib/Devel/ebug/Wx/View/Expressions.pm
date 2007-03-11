c802fc7864ae148d98f44e4b556c8965a522b1c2
Mattia Barbon <mbarbon@cpan.org>
2007-03-11 16:46:12 +0000

    Usability improvements: add an expression when hitting enter in the editing control. Allow in-place editing of expressions. Allow deleting expressions.

--- lib/Devel/ebug/Wx/View/Expressions.pm
+++ lib/Devel/ebug/Wx/View/Expressions.pm
@@ -3,28 +3,46 @@ package Devel::ebug::Wx::View::Expressions;
 use strict;
 use base qw(Wx::Panel Devel::ebug::Wx::View::Base);
 
-# expressions => ARRAY, _expressions ref to expressions
 # FIXME: ought to be a service, too
-__PACKAGE__->mk_accessors( qw(tree expressions) );
+__PACKAGE__->mk_accessors( qw(tree _expressions) );
 
-use Wx qw(:treectrl :sizer);
-use Wx::Event qw(EVT_BUTTON EVT_TREE_ITEM_EXPANDING);
+use Wx qw(:treectrl :textctrl :sizer WXK_DELETE);
+use Wx::Event qw(EVT_BUTTON EVT_TREE_ITEM_EXPANDING EVT_TEXT_ENTER
+                 EVT_TREE_BEGIN_LABEL_EDIT EVT_TREE_END_LABEL_EDIT
+                 EVT_TREE_KEY_DOWN);
 
 sub tag         { 'expressions' }
 sub description { 'Expressions' }
+
+sub expressions { @{$_[0]->_expressions} }
+
+# FIXME backport to wxPerl
+sub _call_on_idle($&) {
+    my( $window, $code ) = @_;
+
+    use Wx::Event qw(EVT_IDLE);
+    # Disconnecting like this is unsafe...
+    my $callback = sub {
+        EVT_IDLE( $window, undef );
+        $code->();
+    };
+    EVT_IDLE( $window, $callback );
+}
 
 sub new {
     my( $class, $parent, $wxebug ) = @_;
     my $self = $class->SUPER::new( $parent, -1 );
 
     $self->wxebug( $wxebug );
-    $self->expressions( [] );
+    $self->_expressions( [] );
     $self->{tree} = Wx::TreeCtrl->new( $self, -1, [-1,-1], [-1,-1],
-                                       wxTR_HIDE_ROOT|wxTR_HAS_BUTTONS );
+                                       wxTR_HIDE_ROOT | wxTR_HAS_BUTTONS |
+                                       wxTR_EDIT_LABELS );
 
     my $refresh = Wx::Button->new( $self, -1, 'Refresh' );
     my $add = Wx::Button->new( $self, -1, 'Add' );
-    my $expression = Wx::TextCtrl->new( $self, -1, '' );
+    my $expression = Wx::TextCtrl->new( $self, -1, '', [-1, -1], [-1, -1],
+                                        wxTE_PROCESS_ENTER );
 
     my $sz = Wx::BoxSizer->new( wxVERTICAL );
     my $cntrl = Wx::BoxSizer->new( wxHORIZONTAL );
@@ -35,14 +53,20 @@ sub new {
     $sz->Add( $self->tree, 1, wxGROW );
     $self->SetSizer( $sz );
 
+    $self->subscribe_ebug( 'state_changed', sub { $self->_refresh( @_ ) } );
     $self->register_view;
     $self->tree->AddRoot( '' );
 
-    EVT_TREE_ITEM_EXPANDING( $self, $self->tree, \&OnExpand );
-    EVT_BUTTON( $self, $refresh, sub { $self->Refresh } );
-    EVT_BUTTON( $self, $add, sub { 
+    EVT_TREE_ITEM_EXPANDING( $self, $self->tree, \&_on_expand );
+    EVT_BUTTON( $self, $refresh, sub { $self->refresh } );
+    EVT_BUTTON( $self, $add, sub {
                     $self->add_expression( $expression->GetValue );
                 } );
+    EVT_TEXT_ENTER( $self, $expression,
+                    sub { $self->add_expression( $expression->GetValue ) } );
+    EVT_TREE_BEGIN_LABEL_EDIT( $self, $self->tree, \&_begin_edit );
+    EVT_TREE_END_LABEL_EDIT( $self, $self->tree, \&_end_edit );
+    EVT_TREE_KEY_DOWN( $self, $self->tree, \&_key_down );
 
     $self->SetSize( $self->default_size );
 
@@ -52,19 +76,51 @@ sub new {
 sub add_expression {
     my( $self, $expression ) = @_;
 
-    push @{$self->expressions}, { expression => $expression,
-                                  level      => 0,
-                                  index      => @{$self->expressions},
-                                  };
-    $self->Refresh;
+    push @{$self->_expressions}, { expression => $expression,
+                                   level      => 0,
+                                   };
+    $self->refresh;
 }
 
-# FIXME global variable
-# FIXME handle collapsing (reduce level il all siblings are collapsed)
-our $processing;
-sub OnExpand {
+sub _is_expression {
+    return $_[0]->GetItemParent( $_[1] ) == $_[0]->GetRootItem;
+}
+
+sub _key_down {
     my( $self, $event ) = @_;
-    return if $processing;
+
+    return unless $event->GetKeyCode == WXK_DELETE;
+    my $item = $event->GetItem || $self->tree->GetSelection;
+    return unless _is_expression( $self->tree, $item );
+    my $expression = $self->tree->GetPlData( $item );
+    $self->_expressions( [ grep $_ ne $expression, $self->expressions ] );
+    _call_on_idle $self, sub { $self->refresh };
+}
+
+# only allow editing root items
+sub _begin_edit {
+    my( $self, $event ) = @_;
+    my $tree = $self->tree;
+
+    if( !_is_expression( $tree, $event->GetItem ) ) {
+        $event->Veto;
+    } else {
+        my $expr = $tree->GetPlData( $event->GetItem )->{expression};
+        $tree->SetItemText( $event->GetItem, $expr );
+    }
+}
+
+sub _end_edit {
+    my( $self, $event ) = @_;
+
+    $self->tree->GetPlData( $event->GetItem )->{expression} = $event->GetLabel;
+    _call_on_idle $self, sub { $self->refresh };
+}
+
+sub _on_expand {
+    my( $self, $event ) = @_;
+    return if $self->{_expanding}; # avoid processing while in refresh
+
     my( $item ) = $event->GetItem;
     my( $root, $expr_item, $level ) = ( $self->tree->GetRootItem, $item, 1 );
     while( $root != ( $item = $self->tree->GetItemParent( $item ) ) ) {
@@ -72,16 +128,22 @@ sub OnExpand {
         $expr_item = $item;
     }
     $self->tree->GetPlData( $expr_item )->{level} = $level;
-    $self->Refresh;
+    $self->refresh;
 }
 
-sub Refresh {
+sub _refresh {
+    my( $self, $ebug, $event, %params ) = @_;
+
+    $self->refresh;
+}
+
+sub refresh {
     my( $self ) = @_;
     my $tree = $self->tree;
 
     my $root = $tree->GetRootItem;
     $tree->DeleteChildren( $root );
-    foreach my $e ( @{$self->expressions} ) {
+    foreach my $e ( $self->expressions ) {
         my $child = $tree->AppendItem( $root, $e->{expression}, -1, -1,
                                        Wx::TreeItemData->new( $e ) );
         my( $val, $ex ) = $self->ebug->eval_level( $e->{expression},
@@ -98,14 +160,14 @@ sub Refresh {
 
 sub _add_childs {
     my( $self, $item, $data, $level ) = @_;
-    local $processing = $processing + 1;
+    local $self->{_expanding} = $self->{_expanding} + 1;
     my( $elts ) = $data->{keys};
     $self->tree->DeleteChildren( $item );
     foreach my $el ( @$elts ) {
         my $child = $self->tree->AppendItem( $item, $el->[0] . ' => ' . $el->[1]->{string} );
         $self->_add_childs( $child, $el->[1], $level ) if $el->[1]{keys};
     }
-    $self->tree->Expand( $item ) if $processing <= $level;
+    $self->tree->Expand( $item ) if $self->{_expanding} <= $level;
 }
 
 1;
