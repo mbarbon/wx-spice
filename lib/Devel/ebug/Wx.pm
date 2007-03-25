6ce477a1891c869247468faed14520595500c853
Mattia Barbon <mbarbon@cpan.org>
2007-03-25 18:19:23 +0000

    Preparing release 0.06.

--- lib/Devel/ebug/Wx.pm
+++ lib/Devel/ebug/Wx.pm
@@ -6,7 +6,7 @@ use strict;
 use warnings;
 use base qw(Wx::Frame Devel::ebug::Wx::Service::Base Class::Accessor::Fast);
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 use Wx qw(:aui wxOK);
 use Wx::Event qw(EVT_CLOSE);
