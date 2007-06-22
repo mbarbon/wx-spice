b0707db7c6f74904e5461de909c7256c5d6cc00d
Mattia Barbon <mbarbon@cpan.org>
2007-06-22 20:14:40 +0000

    Rename EVT_SPICE_UPDATE_UI to EVT_SPICE_UPDATE_UI_ENABLE.   Add EVT_SPICE_UPDATE_UI, EVT_SPICE_MENU.

--- lib/Wx/Spice/UI/Events.pm
+++ lib/Wx/Spice/UI/Events.pm
@@ -4,17 +4,29 @@ use strict;
 use Exporter; *import = \&Exporter::import;
 
 use Wx;
-use Wx::Event qw(EVT_BUTTON EVT_UPDATE_UI);
+use Wx::Event qw(EVT_BUTTON EVT_MENU EVT_UPDATE_UI);
 
-our @EXPORT_OK = qw(EVT_SPICE_BUTTON EVT_SPICE_UPDATE_UI);
+our @EXPORT_OK = qw(EVT_SPICE_BUTTON EVT_SPICE_MENU EVT_SPICE_UPDATE_UI
+                    EVT_SPICE_UPDATE_UI_ENABLE);
+
+sub EVT_SPICE_MENU($$$$) {
+    my $cm = $_[2]->command_manager_service;
+    EVT_MENU( $_[0], $_[1], $cm->get_command_sender( $_[3], $_[2] ) );
+}
 
 sub EVT_SPICE_BUTTON($$$$) {
-    my $cm = $_[2]->get_service( 'command_manager' );
+    my $cm = $_[2]->command_manager_service;
     EVT_BUTTON( $_[0], $_[1], $cm->get_command_sender( $_[3], $_[2] ) );
 }
 
 sub EVT_SPICE_UPDATE_UI($$$$) {
-    my( $cm, $command ) = ( $_[2]->get_service( 'command_manager' ), $_[3] );
+    my( $sm, $sub ) = ( $_[2], $_[3] );
+    my $update_ui = sub { $sub->( $sm, $_[1] ) };
+    EVT_UPDATE_UI( $_[0], $_[1], $update_ui );
+}
+
+sub EVT_SPICE_UPDATE_UI_ENABLE($$$$) {
+    my( $cm, $command ) = ( $_[2]->command_manager_service, $_[3] );
     my $update_ui = sub {
         $_[1]->Enable( $cm->command_active( $command ) );
     };
